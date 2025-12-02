1c8d56c51a2fe3f2f8294be12a52891ac2b39649
Justin <jtsang87@uwo.ca>
2025-12-01 20:51:49 -0500

    Add cors to simulate

--- src/main/java/com/ttsudio/alphaback/simulate/SimulateApplication.java
+++ src/main/java/com/ttsudio/alphaback/simulate/SimulateApplication.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -180,6 +181,7 @@ public class SimulateApplication {
         }
     }
 
+    @CrossOrigin
     @PostMapping(path = "/simulate", produces = MediaType.APPLICATION_JSON_VALUE)
     public SimulationResponse simulate(
             @RequestParam(value="stocks", defaultValue = "GOOGL, AAPL, NVDA") List<String> stocks,
